10ccd8c8aa9b51a2fb49b8c340312d36106b4e79
EnJiang <315067671@qq.com>
2017-03-27 19:51:50 +0800

    V2.0. Change test.py to consoleTest.py, add PyGameTest.py, using pygame to implemente a window.

--- Game.py
+++ Game.py
@@ -1,34 +1,108 @@
-#coding:utf-8
+# coding:utf-8
+
+import pygame
+from pygame.locals import *
+
+from time import sleep
+
 
 class BasicGame(object):
-  """基本游戏逻辑"""
-  def __init__(self):
-    super(BasicGame, self).__init__()
-    self.isTargetSafe = True
+    """基本游戏逻辑"""
 
-  def setMap(self, gameMap):
-    self.map = gameMap
+    def __init__(self):
+        super(BasicGame, self).__init__()
+        self.isTargetSafe = True
 
-  def update(self):
-    for ship in self.map.ships:
-      ship.move()
-    self.checkTarget()
+    def setMap(self, gameMap):
+        self.map = gameMap
 
-  def checkTarget(self):
-    targetX, targetY = self.map.targetCoordinate()
-    for ship in self.map.enemyShips:
-      shipX, shipY = ship.coordinate()
-      if(shipX==targetX and shipY==targetY):
-        self.isTargetSafe = False
+    def update(self):
+        for ship in self.map.ships:
+            ship.move()
+        self.checkTarget()
 
-  def isGameOver(self):
-    return not self.isTargetSafe
+    def checkTarget(self):
+        targetX, targetY = self.map.targetCoordinate()
+        for ship in self.map.enemyShips:
+            shipX, shipY = ship.coordinate()
+            if(shipX == targetX and shipY == targetY):
+                self.isTargetSafe = False
 
-  def start(self):
-    while not self.isGameOver():
-      self.update()
-      print self.map
-      print '----------------------------------------------------------------------------------------'
-      print "press any key to continue"
-      raw_input()
-    print "you lost!"
+    def isGameOver(self):
+        return not self.isTargetSafe
+
+    def start(self):
+        while not self.isGameOver():
+            self.update()
+            print self.map
+            print '----------------------------------------------------------------------------------------'
+            print "press any key to continue"
+            raw_input()
+        print "you lost!"
+
+
+class GUIGame(BasicGame):
+    """docstring for GUIGame"""
+
+    def __init__(self):
+        super(GUIGame, self).__init__()
+        self.gui = pygame
+        self.guiInit()
+
+    def guiInit(self):
+        self.guiScreen = self.gui.display.set_mode((800, 600), 0, 32)
+        self.gui.display.set_caption("USV")
+        self.guiBackground = self.gui.image.load(
+            'src/img/bg/seaSurface.png').convert()
+        self.guiFriendlyShip = self.gui.image.load(
+            'src/img/usv/friendly.png').convert_alpha()
+        self.guiEnemyShip = self.gui.image.load(
+            'src/img/usv/enemy.png').convert_alpha()
+        self.guiTarget = self.gui.image.load(
+            'src/img/target/blueTarget.png').convert_alpha()
+
+    def update(self):
+        xUnit = 800.0 / self.map.width
+        yUnit = 600.0 / self.map.height
+
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                exit()
+
+        self.guiScreen.blit(self.guiBackground, (0, 0))
+
+        for ship in self.map.ships:
+            ship.move()
+            shipX, shipY = ship.coordinate()
+            shipW, shipH = 32, 32
+            if(ship.isEnemy):
+                guiEnemyShip = self.gui.transform.rotate(
+                    self.guiEnemyShip, -ship.direction)
+                self.guiScreen.blit(
+                    guiEnemyShip, (shipX * xUnit - shipW / 2, shipY * yUnit - shipH / 2))
+            else:
+                guiFriendlyShip = self.gui.transform.rotate(
+                    self.guiFriendlyShip, -ship.direction)
+                self.guiScreen.blit(
+                    guiFriendlyShip, (shipX * xUnit - shipW / 2, shipY * yUnit - shipH / 2))
+
+        self.checkTarget()
+
+        targetX, targetY = self.map.targetCoordinate()
+        targetW, targetH = 32, 32
+        self.guiScreen.blit(
+            self.guiTarget, (targetX * xUnit - targetW / 2, targetY * yUnit - targetH / 2))
+
+        self.gui.display.update()
+
+    def start(self):
+        while not self.isGameOver():
+            self.update()
+            sleep(0.5)
+
+        self.gui.display.set_caption("Game Over!")
+        while True:
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    exit()
+            self.gui.display.update()
--- Map.py
+++ Map.py
@@ -1,55 +1,58 @@
-#coding:utf-8
+# coding:utf-8
 
 import copy
 
+
 class BasicMap(object):
-  '''Map类描绘了当前整个海面上的状况
-  在这个简单的字符界面中"~"代表空白海面,E代表敌方舰船,F代表友方舰船'''
-  def __init__(self, width, height):
-    super(BasicMap, self).__init__()
-    self.width, self.height = width, height
-    self.targetX = 0.0
-    self.targetY = 0.0
-    self.friendlyShips = []
-    self.enemyShips = []
-    self.ships = []
+    '''Map类描绘了当前整个海面上的状况
+    在这个简单的字符界面中"~"代表空白海面,E代表敌方舰船,F代表友方舰船'''
 
-  def setTarget(self, x, y):
-    self.targetX, self.targetY = x, y
+    def __init__(self, width, height):
+        super(BasicMap, self).__init__()
+        self.width, self.height = width, height
+        self.targetX = 0.0
+        self.targetY = 0.0
+        self.friendlyShips = []
+        self.enemyShips = []
+        self.ships = []
 
-  def targetCoordinate(self):
-    return self.targetX, self.targetY
+    def setTarget(self, x, y):
+        self.targetX, self.targetY = x, y
 
-  def addShip(self, ship):
-    if(ship.isEnemy):
-      self.enemyShips.append(ship)
-    else:
-      self.friendlyShips.append(ship)
-    self.ships.append(ship)
+    def targetCoordinate(self):
+        return self.targetX, self.targetY
 
-  def __str__(self):
-    _str_ = ""
+    def addShip(self, ship):
+        if(ship.isEnemy):
+            self.enemyShips.append(ship)
+        else:
+            self.friendlyShips.append(ship)
+        self.ships.append(ship)
 
-    matrix = [['~' for i in range(self.width)] for j in range(self.height)]
-    matrix[self.targetY][self.targetX] = 'T'
-    for ship in self.ships:
-      shipX, shipY = ship.coordinate()
-      if(ship.isEnemy):
-        matrix[shipY][shipX] = 'E'
-      else:
-        matrix[shipY][shipX] = 'F'
+    def __str__(self):
+        _str_ = ""
 
-    for line in matrix:
-      for one in line:
-        _str_ += one + ' '
-      _str_ += '\n'
+        matrix = [['~' for i in range(self.width)] for j in range(self.height)]
+        matrix[self.targetY][self.targetX] = 'T'
+        for ship in self.ships:
+            shipX, shipY = ship.coordinate()
+            if(ship.isEnemy):
+                matrix[shipY][shipX] = 'E'
+            else:
+                matrix[shipY][shipX] = 'F'
 
-    return _str_
+        for line in matrix:
+            for one in line:
+                _str_ += one + ' '
+            _str_ += '\n'
+
+        return _str_
 
 
 class LargeMap(BasicMap):
-  """一个较大的地图,用大型矩阵来描绘一个伪二维连续平面
-  每次USV移动之后,LargeMap会对USV的位置进行校正,将其浮点区域去掉并
-  定位到最近的那一个矩阵点"""
-  def __init__(self, width, height):
-    super(LargeMap, self).__init__(width, height)
+    """一个较大的地图,用大型矩阵来描绘一个伪二维连续平面
+    每次USV移动之后,LargeMap会对USV的位置进行校正,将其浮点区域去掉并
+    定位到最近的那一个矩阵点"""
+
+    def __init__(self, width, height):
+        super(LargeMap, self).__init__(width, height)
