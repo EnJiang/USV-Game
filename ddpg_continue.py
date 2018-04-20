877b936ad578eb0bd8dd252be8d0682654732067
EnJiang <315067671@qq.com>
2018-04-20 17:05:51 +0800

    update warm up

--- ddpg_continue.py
+++ ddpg_continue.py
@@ -88,12 +88,12 @@ if __name__ == "__main__":
         self.recent_observation = observation
         self.recent_action = action
         return action
-        
+
     DDPGAgent.forward = monkey_patching_forward
 
     memory = SequentialMemory(limit=100000, window_length=1)
     agent = DDPGAgent(nb_actions=1, actor=actor, critic=critic, critic_action_input=action_input,
-                    memory=memory, nb_steps_warmup_critic=1000, nb_steps_warmup_actor=1000,
+                    memory=memory, nb_steps_warmup_critic=8000, nb_steps_warmup_actor=8000,
                     gamma=.99, target_model_update=1e-3)
 
     agent.compile([Adam(lr=1e-4), Adam(lr=1e-3)], metrics=['mae'])
